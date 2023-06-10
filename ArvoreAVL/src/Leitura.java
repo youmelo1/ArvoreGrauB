4fa6ec80302f7bc1dade669c12d80f94b87cf05d
Your Name <you@example.com>
2023-06-10 13:29:07 -0300

    adicionando leitura de arquivo

---
+++ ArvoreAVL/src/Leitura.java
@@ -0,0 +1,41 @@
+import java.io.IOException;
+import java.util.Scanner;
+import java.io.File;
+
+public class Leitura 
+{
+    public res[] read(File arq) 
+    {       
+        File file = arq;
+        res[] r = new res[100];
+        String[] nomes = new String[100];
+        String[] cpf = new String[100];
+        String[] data = new String[100];
+        int i = 0;
+        try{
+            Scanner sc = new Scanner(file);
+           
+            while(sc.hasNextLine())
+            {   
+                
+                String leitura = sc.nextLine();
+                String[] a = leitura.split(";");
+                res re = new res(a[0], a[1], a[2]);
+                r[i] = re;
+                nomes[i] = r[i].nome;
+                cpf[i] = r[i].cpf;
+                data[i] = r[i].data;
+                System.out.println(nomes[i]);
+                System.out.println(r[i].nome);
+                System.out.println(r[i].cpf);
+                System.out.println(r[i].data);
+            }
+        } catch (IOException e)
+        {
+            System.out.println(e.getMessage());
+        } 
+
+        return r;
+    }
+
+}
---
+++ ArvoreAVL/src/res.java
@@ -0,0 +1,12 @@
+public class res {
+    String nome;
+    String cpf;
+    String data;
+
+
+    public res(String nome, String cpf, String data) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.data = data;
+    }
+}
