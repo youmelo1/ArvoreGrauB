77f3af48a60da83b3aa6fc7088292d1e0ba84965
Rodrigo <113945542+youmelo1@users.noreply.github.com>
2023-06-15 18:13:56 -0300

    Add files via upload

---
+++ ArvoreAVLFinal/src/main/Principal.java
@@ -0,0 +1,11 @@
+package main;
+import java.text.ParseException;
+
+import ui.Menu;
+
+public class Principal {
+	public static void main(String[] args){
+		Menu menu = new Menu();
+		menu.menu();
+	}
+}
