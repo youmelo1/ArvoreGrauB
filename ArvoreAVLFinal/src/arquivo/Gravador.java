69cc2f1f2431b371dee2557e457111dee4a460fd
Rodrigo <113945542+youmelo1@users.noreply.github.com>
2023-06-15 18:16:53 -0300

    Update Gravador.java

--- ArvoreAVLFinal/src/arquivo/Gravador.java
+++ ArvoreAVLFinal/src/arquivo/Gravador.java
@@ -6,7 +6,7 @@ public class Gravador {
 	    String data;
 	    String cidade;
 
-	    public Gravador (String nome, String cpf, String rg, String data, String cidade){
+	    public Gravador (String cpf, String rg, String nome, String data, String cidade){
 	        this.cpf = cpf;
 	        this.rg = rg;
 	        this.nome = nome;
